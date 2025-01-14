2407d90772a81c0dc31369ca8caf666d9020f130
Rodrigo Palomo <repmlm@hotmail.com>
2025-01-14 17:02:26 -0300

    finish dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/ui/button";
 import DashboardCard from "@/components/dashboard/DashboardCard";
 import { Folder, MessageCircle, Newspaper, User } from "lucide-react";
 import PostsTable from "@/components/posts/PostsTable";
+import AnalyticsChart from "@/components/dashboard/AnalyticsChart";
 
 
 export default function Home() {
@@ -29,6 +30,7 @@ export default function Home() {
           icon={<MessageCircle className="text-slate-500" size={72} />}
         />
       </div>
+      <AnalyticsChart />
       <PostsTable title="Latest Posts" limit={5} />
     </>
   );
